dc8662e4c29d57b1f45e8e4545d244dd0232e774
efegorkemumit <47424323+efegorkemumit@users.noreply.github.com>
2023-09-28 20:59:49 +0300

    Header 5
    Youtube Efe Görkem Ümit

--- src/js/script.js
+++ src/js/script.js
@@ -1,14 +1,45 @@
 const userMenuButton = document.getElementById('user-menu-button');
 const userMenu = document.getElementById('user-menu');
 
-userMenuButton.addEventListener('click', ()=> {
-    userMenu.classList.toggle('hidden');
-})
-
-
 const userMenuButtonInbox = document.getElementById('user-menu-inbox-button');
 const userMenuInbox = document.getElementById('user-menu-Inbox');
 
-userMenuButtonInbox.addEventListener('click', ()=> {
-    userMenuInbox.classList.toggle('hidden');
+const userMenuButtonBell = document.getElementById('user-menu-bell-button');
+const userMenuBell = document.getElementById('user-menu-bell');
+
+
+function closeUserMenus(){
+    userMenu.classList.add('hidden');
+    userMenuInbox.classList.add('hidden');
+    userMenuBell.classList.add('hidden');
+}
+
+
+function toggleUserMenu(menu){
+    if(menu.classList.contains('hidden')){
+        closeUserMenus();
+        menu.classList.remove('hidden')
+    }
+    else{
+        menu.classList.add('hidden')
+    }
+
+}
+
+
+userMenuButton.addEventListener('click', ()=> {
+    toggleUserMenu(userMenu)
 })
+
+
+userMenuButtonInbox.addEventListener('click', ()=> {
+    toggleUserMenu(userMenuInbox)
+})
+
+userMenuButtonBell.addEventListener('click', ()=> {
+    toggleUserMenu(userMenuBell)
+})
+
+
+
+
