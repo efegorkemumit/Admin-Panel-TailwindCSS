0cb16fb23c8b0ea6d888aefbf64b3bc425f3b4d0
efegorkemumit <47424323+efegorkemumit@users.noreply.github.com>
2023-09-28 16:23:47 +0300

    Fonts
    Youtube Efe Görkem Ümit

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@
 module.exports = {
   content: ["*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily:{
+        'poppins':['Poppins'],
+        'roboto' :['Roboto']
+      }
+    },
   },
   plugins: [],
 }
