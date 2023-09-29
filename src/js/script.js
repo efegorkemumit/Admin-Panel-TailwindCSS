27bfd2091e71ada467f34a7f81c347a3dde5f58d
efegorkemumit <47424323+efegorkemumit@users.noreply.github.com>
2023-09-29 16:46:05 +0300

    Mobile menu 2
    Youtube Efe Görkem Ümit

--- src/js/script.js
+++ src/js/script.js
@@ -52,3 +52,16 @@ document.addEventListener("DOMContentLoaded", function(){
 });
 
 
+let menuBar = document.querySelector('#menuBar')
+let mobileMenu = document.querySelector('#mobileMenu')
+let closeMenu = document.querySelector('#closeMenu')
+
+menuBar.addEventListener('click', function(){
+    mobileMenu.classList.remove('hidden')
+})
+
+closeMenu.addEventListener('click', function(){
+    mobileMenu.classList.add('hidden')
+})
+
+
